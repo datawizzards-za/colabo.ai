2cc0f42cda43d0c1d8674240104bd4e29662209f
LERO <ofentswel@gmail.com>
2017-09-30 23:27:00 +0200

    anged the templated for recomendation for topics

--- app/views.py
+++ app/views.py
@@ -3,14 +3,21 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.views import View
-# Create your views here.
+from rest_framework import generics
+
+from app.utils import topics
+import pickle
 
 
 class Home(View):
     template_name = 'app/index.html'
 
     def get(self, request):
-        return render(request, self.template_name)
+        data = pickle.load(open('data/johannesburg.pkl', 'r'))
+        data_samples = [d['description'] for d in data]
+        headlines = topics.compute_nmf(data_samples)
+        context = {'headlines': headlines}
+        return render(request, self.template_name, context)
 
     def post(self, request, args, **kwarg):
         return render(request, self.template_name)
@@ -34,3 +41,16 @@ class SignUp(View):
 
     def post(self, request, args, **kwarg):
         return render(request, self.template_name)
+
+
+############################## API VIEWS #####################################
+
+
+class GetSystemOrderDetails(generics.ListAPIView):
+    def get_queryset(self):
+        order_number = self.kwargs['user_number']
+        data = pickle.load(open('data/johannesburg.pkl', 'r'))
+        data_samples = [d['description'] for d in data]
+        headlines = topics.compute_nmf(data_samples)
+        context = {'headlines': headlines}
+        return context
