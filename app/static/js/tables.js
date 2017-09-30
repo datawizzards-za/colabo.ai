3ed3925e3884d6c0be70778c729eaf470b680320
Mabu <manailengmj@gmail.com>
2017-09-30 15:58:24 +0200

    UPDATE(ui): layout

--- app/static/js/tables.js
+++ app/static/js/tables.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
+$(document).ready(function () {
         $('#example').dataTable();
-} );
+        $('#requests_view_tab').dataTable();
+        $('#projects_view_tab').dataTable();
+        $('#meetcoll_view_tab').dataTable();
+        $('#collmeet_view_tab').dataTable();
+        $('#insights_view_tab').dataTable();
+});
---
+++ app/static/js/views.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+        $('#requests_view').hide();
+        $('#collmeet_view').hide();
+        $('#meetcoll_view').hide();
+        $('#projects_view').hide();
+        $('#insights_view').hide();
+        $('#project_details').hide();
+
+        $('#projects_view_tab tbody').on('click', 'tr', function (e) {
+                $('#projects_view').hide();
+                $('#project_details').show();
+        });
+
+
+        $('#btn_join_proj').click(function () {
+                $.notify("yay! request send to your line manager for approval.",
+                        "success",
+                        { position: 'right top' });
+        });
+
+        $('#requests').click(function () {
+                $('#dash_view').hide();
+                $('#collmeet_view').hide();
+                $('#meetcoll_view').hide();
+                $('#projects_view').hide();
+                $('#insights_view').hide();
+                $('#project_details').hide();
+                $('#requests_view').show();
+        });
+
+        $('#dashboard').click(function () {
+                $('#requests_view').hide();
+                $('#collmeet_view').hide();
+                $('#meetcoll_view').hide();
+                $('#projects_view').hide();
+                $('#insights_view').hide();
+                $('#project_details').hide();
+                $('#dash_view').show();
+        });
+
+        $('#colleaguemeet').click(function () {
+                $('#dash_view').hide();
+                $('#meetcoll_view').hide();
+                $('#projects_view').hide();
+                $('#requests_view').hide();
+                $('#insights_view').hide();
+                $('#project_details').hide();
+                $('#collmeet_view').show();
+        });
+
+        $('#meetcolleague').click(function () {
+                $('#dash_view').hide();
+                $('#requests_view').hide();
+                $('#collmeet_view').hide();
+                $('#projects_view').hide();
+                $('#insights_view').hide();
+                $('#project_details').hide();
+                $('#meetcoll_view').show();
+        });
+
+        $('#project').click(function () {
+                $('#dash_view').hide();
+                $('#requests_view').hide();
+                $('#collmeet_view').hide();
+                $('#meetcoll_view').hide();
+                $('#insights_view').hide();
+                $('#project_details').hide();
+                $('#projects_view').show();
+        });
+
+        $('#insights').click(function () {
+                $('#dash_view').hide();
+                $('#requests_view').hide();
+                $('#collmeet_view').hide();
+                $('#meetcoll_view').hide();
+                $('#projects_view').hide();
+                $('#project_details').hide();
+                $('#insights_view').show();
+        });
+
+});
