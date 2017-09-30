2021cd31f1d2b8bb98d5e7dc3c7a082022b8c066
Mabu <manailengmj@gmail.com>
2017-09-30 02:18:25 +0200

    ADD: ui template error

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-import views
+from app import views
 
 urlpatterns = [
-    url(r'^\Z', views.Home.as_view(), name='name'),
+    url(r'^\Z', views.Home.as_view(), name='home'),
 ]
